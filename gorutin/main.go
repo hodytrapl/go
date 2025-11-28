621f1512ddc004ed0ed520f9676f201ff01c4849
Efim <jacober@list.ru>
2025-11-28 12:52:38 +0300

    доделал домашку\классную gorutine. обновил конструкцию

--- gorutin/main.go
+++ gorutin/main.go
@@ -1,19 +1,46 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-	"os"
 	"time"
 )
 
-func homework(n int) {
-	for i := 1; i <= n; i++ {
-		fmt.Printf("[gorutine %d] message %s-%d", i, "messss",i)
+const (
+	messagePerGorutine = 3
+	timePerSecond      = 5
+)
+
+/*func homework(id int) {
+	for i := 1; i <= id; i++ {
+		fmt.Printf("[gorutine %d] message %d\n", id, i)
+	}
+}*/
+
+func homework() {
+	for i := 1; i <= messagePerGorutine; i++ {
+		fmt.Printf("[gorutine %d] message %d\n", messagePerGorutine, i)
 	}
 }
 
 func main() {
-	n:=os.Args[0]
-	homework(n)
-	time.Sleep(5 * time.Second)
+	/*if len(os.Args) != 2 {
+		log.Fatal("Ожидается аргумент: количетсво горутин\n")
+	}
+	countgoroutline, err := strconv.Atoi(os.Args[1])
+	if err != nil || countgoroutline <= 0 {
+		log.Fatal("аргумент не положительное число\n")
+	}
+	for i := 0; i < countgoroutline; i++ {
+		go homework(i)
+	}*/
+
+	count := flag.Int("n", 10, "флаг который указывает значение по умолчанию 10")
+	flag.Parse()
+
+	for i := 0; i < *count; i++ {
+		go homework()
+	}
+
+	time.Sleep(time.Duration(*count) * timePerSecond)
 }
