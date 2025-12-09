980329577a900dc65dfbf933c350237b5877aeb1
hodytrapl <kostik290820077@gmail.com>
2025-12-09 13:37:13 +0300

    did labrat 29

--- labrat/29/task1/main.go
+++ labrat/29/task1/main.go
@@ -5,6 +5,32 @@ import (
 	"sync"
 )
 
-func main() {
+type BankAccount struct{
+	mu 		sync.Mutex
+	wg      sync.WaitGroup
+	balance int
+}
 
+func (b *BankAccount) Deposit(amount int) {
+	b.mu.Lock()
+	defer b.mu.Unlock()
+	defer b.wg.Done()
+	b.balance += amount
+}
+
+
+
+func main() {
+	user := BankAccount{balance:0}
+
+	for i:=0;i<1000;i++ {
+		user.wg.Add(1)
+		go func(value int){
+			user.Deposit(value)
+		}(1)
+	}
+
+	user.wg.Wait()
+	
+	fmt.Printf("Баланс после 1000 пополнений: %d\n", user.balance)
 }
--- labrat/29/task2/main.go
+++ labrat/29/task2/main.go
@@ -3,8 +3,71 @@ package main
 import (
 	"fmt"
 	"sync"
+	"math/rand"
+	"strconv"
+	"time"
 )
 
-func main() {
+type PhoneBook struct{
+	mu 		sync.RWMutex
+	wg      sync.WaitGroup
+	contactList map[string]string 
+}
 
+func (pb *PhoneBook) Set(name, phone string){
+	pb.mu.Lock()
+	defer pb.mu.Unlock()
+	fmt.Println("vnosim: ",name)
+	pb.contactList[name]=phone
+}
+
+func (pb *PhoneBook) Get(name string) string{
+	pb.mu.RLock()
+	defer pb.mu.RUnlock()
+	return pb.contactList[name]
+}
+
+func generateName(id int) string {
+	firstNames := []string{"Алексей", "Мария", "Иван", "Ольга", "Дмитрий", "Анна", "Сергей", "Елена", "Андрей", "Наталья"}
+	lastNames := []string{"Иванов", "Петров", "Сидоров", "Смирнов", "Кузнецов", "Попов", "Васильев", "Павлов", "Семенов", "Голубев"}
+	
+	firstName := firstNames[rand.Intn(len(firstNames))]
+	lastName := lastNames[rand.Intn(len(lastNames))]
+	
+	return fmt.Sprintf("%s %s_%d", firstName, lastName, id)
+}
+
+func generatePhoneNumber(regionCode string) string {
+	number := ""
+	for i := 0; i < 10; i++ {
+		number += strconv.Itoa(rand.Intn(10))
+	}
+	return regionCode + " " + number
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	book := PhoneBook{contactList: make(map[string]string)};
+
+	
+	for i:=0;i<1000;i++{
+		book.wg.Add(1)
+		go func(id int){
+			defer book.wg.Done()
+			name := generateName(id)
+			number := generatePhoneNumber("+7")
+			book.Set(name,number)
+		}(i)
+	}
+
+	book.wg.Wait()
+
+	count := 0
+	for name, phone := range book.contactList {
+		fmt.Printf("%s: %s\n", name, phone)
+		count++
+		if count >= 10 {
+			break
+		}
+	}
 }
